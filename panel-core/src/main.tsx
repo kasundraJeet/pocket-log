98700087aaec2f5f326c86f6f72f40b5cb92655d
kasundraJeet <kasundrajeet29@gmail.com>
2025-01-03 22:23:08 +0530

    install react query

--- panel-core/src/main.tsx
+++ panel-core/src/main.tsx
@@ -4,9 +4,17 @@ import LayoutWrapper from "@/components/layout/LayoutWrapper";
 import { Toaster } from "@/components/ui/sonner";
 import { ThemeProvider } from "@/context/theme-provider";
 import "./index.css";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import App from "./App.tsx";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById("root")!).render(
+  <QueryClientProvider client={queryClient}>
+  <ReactQueryDevtools initialIsOpen={false} />
   <ThemeProvider storageKey="vite-ui-theme">
     <LayoutWrapper>
       <Toaster />
@@ -17,4 +25,5 @@ createRoot(document.getElementById("root")!).render(
       </BrowserRouter>
     </LayoutWrapper>
   </ThemeProvider>
+  </QueryClientProvider>
 );
